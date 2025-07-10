8f37764121177eb64b19168dde9dec51f0117e31
Kevin brun <kbrun.pro@gmail.com>
2025-07-10 22:39:11 +0200

    Enable Cors + add typeOrm + connection with API

--- apps/bourgad-front/src/environments/environment.ts
+++ apps/bourgad-front/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     name: "dev",
     settings:{
       appName: "Bourgad",
-      backend: "http://localhost:3000",
+      backend: "http://localhost:3000/api",
       frontPage: "http:localhost:4200"
     }
   };
