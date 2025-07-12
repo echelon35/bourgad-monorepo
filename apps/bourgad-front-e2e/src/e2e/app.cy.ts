291cbb631708da7529bf16eff4b840969e376ef5
echelon35 <kbrun.pro@gmail.com>
2025-07-12 10:32:21 +0200

    First test with Cypress

--- apps/bourgad-front-e2e/src/e2e/app.cy.ts
+++ apps/bourgad-front-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,10 @@
-import { getGreeting } from '../support/app.po';
-
 describe('bourgad-front-e2e', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
+  it('should display feed view', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
+    // cy.login('my-email@something.com', 'myPassword');
+    cy.get('input[name="name"]').should('have.attr', 'placeholder', 'Kevin, que souhaitez-vous partager dans votre Bourgade ?');
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome/);
   });
 });
