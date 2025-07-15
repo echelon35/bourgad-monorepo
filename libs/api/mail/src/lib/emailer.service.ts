9062237037c15a9ba2435186cbb7f8036eeb6bfb
echelon35 <kbrun.pro@gmail.com>
2025-07-15 08:01:08 +0200

    Mail API Lib

---
+++ libs/api/mail/src/lib/emailer.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+import { SESv2Client, SendEmailCommand } from '@aws-sdk/client-sesv2';
+import { ConfigService } from '@nestjs/config';
+
+/**
+ * Service to send alerts to users
+ */
+@Injectable()
+export class EmailerService {
+  private transporter: any;
+
+  constructor(private configService: ConfigService) {
+    const sesClient = new SESv2Client({ region: process.env['AWS_REGION'], apiVersion: '2019-09-27' });
+    this.transporter = nodemailer.createTransport({
+        SES: { sesClient, SendEmailCommand },
+    });
+  }
+
+  async sendEmail(toEmail: string, subject: string, htmlContent: any) {
+    const mailOptions = {
+      from: 'bourgad.noreply@gmail.com', // Email expéditeur
+      to: toEmail, // Destinataire
+      subject: subject, // Sujet du mail
+      html: htmlContent, // Contenu HTML du mail
+    };
+
+    try {
+      await this.transporter.sendMail(mailOptions);
+    } catch (error) {
+      console.error("Erreur lors de l'envoi de l'email :", error);
+    }
+  }
+
+  async sendConfirmationMail(mail: string, url: string) {
+    const mailOptions = {
+      from: 'bourgad.noreply@gmail.com', // Email expéditeur
+      to: mail, // Destinataire
+      subject: 'Confirmez votre adresse mail', // Sujet du mail
+      html: `<p>Veuillez confirmer votre adresse email en cliquant sur le lien suivant :</p>
+             <a href="${url}">Confirmer mon adresse email</a>`,
+    };
+
+    try {
+      await this.transporter.sendMail(mailOptions);
+    } catch (error) {
+      console.error("Erreur lors de l'envoi de l'email :", error);
+    }
+  }
+
+  async sendPasswordReinitialization(mail: string, url: string) {
+    const mailOptions = {
+      from: 'bourgad.noreply@gmail.com', // Email expéditeur
+      to: mail, // Destinataire
+      subject: 'Réinitialisez votre mot de passe', // Sujet du mail
+      html: `<p>Veuillez réinitialiser votre mot de passe en cliquant sur le lien suivant :</p>
+             <a href="${url}">Réinitialiser mon mot de passe</a>
+             <p>Vous n'êtes pas l'auteur de cette ré-initialisation ? Merci de ne pas tenir compte de cet email.</p>`,
+    };
+
+    try {
+      await this.transporter.sendMail(mailOptions);
+    } catch (error) {
+      console.error("Erreur lors de l'envoi de l'email :", error);
+    }
+  }
+}
