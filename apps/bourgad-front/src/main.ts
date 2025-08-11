03dcc1f58a95111e8b142ba6cdfba9e8f136c79a
echelon35 <kbrun.pro@gmail.com>
2025-08-11 22:34:37 +0200

    Diable console log in production

--- apps/bourgad-front/src/main.ts
+++ apps/bourgad-front/src/main.ts
@@ -1,5 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { environment } from './environments/environment';
 
+if(environment.production){
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    window.console.log = () => { }
+}
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
