fee7de81b9082be7a5e2adb23dc11fd2bac4a81e
echelon35 <kbrun.pro@gmail.com>
2025-08-22 11:25:01 +0200

    Reorganize DTOs

--- libs/shared/dto/internal/src/index.ts
+++ libs/shared/dto/internal/src/index.ts
@@ -1,14 +1,31 @@
-export * from './lib/auth/signUp.dto';
-export * from './lib/auth/login.dto';
-export * from './lib/changePassword.dto';
-export * from './lib/googleLogin.dto';
-export * from './lib/media.dto';
-export * from './lib/auth/token.dto';
-export * from './lib/territory/city/getCityByIdParam.dto';
-export * from './lib/territory/city/updateCityByDeptQuery.dto';
-export * from './lib/auth/resendConfirmationMail.dto';
-export * from './lib/subcategories/AddSubcategory.dto';
-export * from './lib/user/getProfile.dto';
+//AUTH LOGIN
+export * from './lib/auth/login/changePassword.dto';
+export * from './lib/auth/login/login.dto';
+export * from './lib/auth/login/token.dto';
+export * from './lib/auth/login/changePassword.dto';
+export * from './lib/auth/login/googleLogin.dto';
+
+//AUTH SIGNIN
+export * from './lib/auth/signup/signUp.dto';
+export * from './lib/auth/signup/resendConfirmationMail.dto';
+
+//CATEGORY
+export * from './lib/category/AddSubcategory.dto'; 
+
+//LOCATION
+export * from './lib/location/createLocation.dto';
+
+//MEDIA
+export * from './lib/media/media.dto';
+
+//POST
 export * from './lib/post/createPost.dto';
 export * from './lib/post/getPostsAround.dto';
-export * from './lib/location/createLocation.dto';
+export * from './lib/post/feedPost.dto';
+
+//TERRITORY
+export * from './lib/territory/city/getCityByIdParam.dto';
+export * from './lib/territory/city/updateCityByDeptQuery.dto';
+
+//USER
+export * from './lib/user/getProfile.dto'; 
